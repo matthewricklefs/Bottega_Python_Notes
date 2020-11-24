9d77543a0d3fcccb34a3f5bdf64a1cfd9b2a0ffe
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-11-24 22:23:40 +0000

    properties and decorators, dunder methods

--- bottega_curriculum/classes.py
+++ bottega_curriculum/classes.py
@@ -29,21 +29,73 @@
 
 #How to Get and Set Data in a Python Class
 
+# class Invoice:
+
+#     def __init__(self, client, total):
+#         self.client = client
+#         self.total = total
+
+#     def formatter(self):
+#         return f'{self.client} owes: ${self.total}'
+
+
+# google = Invoice('Google', 100)
+
+# print(google.client)
+
+# google.client = 'Yahoo'
+
+# print(google.client) 
+
+# How to Work with Python Properties and Decorators
+
+# class Invoice:
+
+#     def __init__(self, client, total):
+#         self._client = client
+#         self._total = total
+
+#     def formatter(self):
+#         return f'{self._client} owes: ${self._total}'
+
+#     @property
+#     def client(self):
+#         return self._client
+
+#     @client.setter
+#     def client(self, client):
+#         self._client = client
+
+#     @property
+#     def total(self):
+#         return self._total
+
+# google = Invoice('Google', 100)
+
+# print(google.client)
+
+# google.client = 'Yahoo'
+
+# print(google.client)
+
+#Overview of Dunder Methods in Python: __init__
+
 class Invoice:
-
-    def __init__(self, client, total):
-        self.client = client
-        self.total = total
-
-    def formatter(self):
-        return f'{self.client} owes: ${self.total}'
+  def __str__(self):
+    return "This is the invoice class!"
 
 
-google = Invoice('Google', 100)
+inv = Invoice()
+print(str(inv))
 
-print(google.client)
+class Invoice:
+  def __init__(self, client, total):
+    self.client = client
+    self.total = total
 
-google.client = 'Yahoo'
+  def __str__(self):
+    return f"Invoice from {self.client} for {self.total}"
 
-print(google.client) 
 
+inv = Invoice('Google', 500)
+print(str(inv))

c748d00b0190f79f2099d74f68a8184bbe3adfd1
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-11-24 23:25:07 +0000

    presenting a custom iterator

--- bottega_curriculum/classes.py
+++ bottega_curriculum/classes.py
@@ -80,22 +80,104 @@
 
 #Overview of Dunder Methods in Python: __init__
 
-class Invoice:
-  def __str__(self):
-    return "This is the invoice class!"
+# class Invoice:
+#   def __str__(self):
+#     return "This is the invoice class!"
 
 
-inv = Invoice()
-print(str(inv))
+# inv = Invoice()
+# print(str(inv))
 
-class Invoice:
-  def __init__(self, client, total):
-    self.client = client
-    self.total = total
+# class Invoice:
+#   def __init__(self, client, total):
+#     self.client = client
+#     self.total = total
 
-  def __str__(self):
-    return f"Invoice from {self.client} for {self.total}"
+#   def __str__(self):
+#     return f"Invoice from {self.client} for {self.total}"
 
 
-inv = Invoice('Google', 500)
-print(str(inv))
+# inv = Invoice('Google', 500)
+# print(str(inv))
+
+###dunder repr
+
+# class Invoice:
+#   def __init__(self, client, total):
+#     self.client = client
+#     self.total = total
+
+#   def __str__(self):
+#     return f"Invoice from {self.client} for {self.total}"
+
+#   def __repr__(self):
+#     return f"Invoice(<{self.client}, total : {self.total}>)"
+
+
+# inv = Invoice('Google', 500)
+# print(str(inv))
+# print(repr(inv))
+
+###Building a Customer Iterator
+
+class Lineup:
+
+    def __init__(self, players):
+        self.players = players
+        self.last_player_index = (len(self.players) - 1)
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def get_player(self, n):
+        return self.players[n]
+
+    def __next__(self):
+        if self.n < self.last_player_index:
+            player = self.get_player(self.n)
+            self.n += 1
+            return player
+        elif self.n == self.last_player_index:
+            player = self.get_player(self.n)
+            self.n = 0
+            return player
+
+
+astros = [
+  'Springer',
+  'Bregman',
+  'Altuve',
+  'Correa',
+  'Reddick',
+  'Gonzalez',
+  'McCann',
+  'Davis',
+  'Tucker'
+]
+
+astros_lineup = Lineup(astros)
+process = iter(astros_lineup)
+
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))

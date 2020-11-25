18b6d78f567b89bb584f822678f1b920ce1820fd
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-11-25 00:01:24 +0000

    generator

--- bottega_curriculum/classes.py
+++ bottega_curriculum/classes.py
@@ -120,28 +120,72 @@
 
 ###Building a Customer Iterator
 
-class Lineup:
+# class Lineup:
 
+#     def __init__(self, players):
+#         self.players = players
+#         self.last_player_index = (len(self.players) - 1)
+
+#     def __iter__(self):
+#         self.n = 0
+#         return self
+
+#     def get_player(self, n):
+#         return self.players[n]
+
+#     def __next__(self):
+#         if self.n < self.last_player_index:
+#             player = self.get_player(self.n)
+#             self.n += 1
+#             return player
+#         elif self.n == self.last_player_index:
+#             player = self.get_player(self.n)
+#             self.n = 0
+#             return player
+
+
+# astros = [
+#   'Springer',
+#   'Bregman',
+#   'Altuve',
+#   'Correa',
+#   'Reddick',
+#   'Gonzalez',
+#   'McCann',
+#   'Davis',
+#   'Tucker'
+# ]
+
+# astros_lineup = Lineup(astros)
+# process = iter(astros_lineup)
+
+# print(next(process))
+
+
+#### GENERATOR
+
+class InfiniteLineup:
     def __init__(self, players):
         self.players = players
-        self.last_player_index = (len(self.players) - 1)
 
-    def __iter__(self):
-        self.n = 0
-        return self
+    def lineup(self):
+        lineup_max = len(self.players)
+        idx = 0
 
-    def get_player(self, n):
-        return self.players[n]
+        while True:
+            if idx < lineup_max:
+                yield self.players[idx]
+            else:
+                idx = 0
+                yield self.players[idx]
 
-    def __next__(self):
-        if self.n < self.last_player_index:
-            player = self.get_player(self.n)
-            self.n += 1
-            return player
-        elif self.n == self.last_player_index:
-            player = self.get_player(self.n)
-            self.n = 0
-            return player
+            idx += 1
+
+    def __repr__(self):
+        return f'<InfiniteLineup({self.players})'
+
+    def __str__(self):
+        return f"InfiniteLineup with the players: {', '.join(self.players)}"
 
 
 astros = [
@@ -156,28 +200,22 @@ astros = [
   'Tucker'
 ]
 
-astros_lineup = Lineup(astros)
-process = iter(astros_lineup)
+full_lineup = InfiniteLineup(astros)
+astros_lineup = full_lineup.lineup()
 
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+
+print(repr(full_lineup))
+
+print(str(full_lineup))
